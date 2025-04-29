720505a4d7ec84e199106b344ba899cb42613cf4
Domingos Pinho <domingospinho@users.noreply.github.com>
2025-04-29 22:06:48 +0000

    Configure git commit user

--- install-script.sh
+++ install-script.sh
@@ -1,11 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# ##!/bin/bash
 
-#set -
+# -e option instructs bash to immediately exit if any command [1] has a non-zero exit status
+# We do not want users to end up with a partially working install, so we exit the script
+# instead of continuing the installation with something broken
+set -e
+
+# Trap any errors, then exit
+trap abort INT QUIT TERM
 
 echo -e "\n\n\n\n\n\n\n\n"
 
+if [ "$( id -u )" -ne 0 ]; then
+    echo -e 'Please run this script as root\n' >&2
+    exit 1
+fi
+
 REDNUMBER=31
 GREENNUMBER=32
+
+INFO="[i]"
 
 RED="\e[31m"
 GREEN="\e[32m"
@@ -14,13 +28,15 @@ ENDCOLOR="\e[0m"
 BOLDGREEN="\e[1;${GREENNUMBER}m"
 ITALICRED="\e[3;${REDNUMBER}m"
 
+whoAmI=$(whoami)
+
+printf "\nCurrent User: %s\n" "$whoAmI"
+
 myExternalIP=$(curl -s ifconfig.me)
 
 echo -e "\nYour external IP address: ${BOLDGREEN}$myExternalIP\e[0m\n"
 
-#echo -e "\n"
 read -p "What is the domain you want to add to the hosts file? " domain
-#echo -e "\n"
 
 echo -e "\n127.0.0.1        ${RED}$domain\e[0m\n"
 
@@ -51,15 +67,35 @@ fi
 
 ##apt-get update
 ##apt-get upgrade
-##
-##apt-get install git
-##
-read -p "What email do you want to set for git commits? " gitEmail
-read -p "What name do you want to set for git commits? " gitName
 
-echo -e "\n$gitEmail\n"
-echo -e "\n$gitName\n"
+read -p "Do you want to install git? (Yes/No) " installGit
 
-$(git config --global user.email gitEmail)
-git config --global user.name '$gitName'
+if [[ $installGit =~ ^[Yy]$ ]] || [[ $installGit == "Yes" ]] || [[ $installGit == "yes" ]]
+then
+    apt-get install git
+fi
 
+read -ep $'\nWhat operating system user will you use to commit? Press enter to ignore. ' commitsUser
+
+if [ -z "$commitsUser" ]
+then
+    echo -e "\n${RED}No operating system user for git defined\e[0m\n"
+else
+    read -p $'\nWhat email do you want to set for git commits? ' gitEmail
+    read -p $'\nWhat name do you want to set for git commits? ' gitName
+
+    su $commitsUser -c "git config --global user.email \"$gitEmail\""
+
+    su $commitsUser -c "git config --global user.name \"$gitName\""
+
+    echo -e "\nGit config set at ${GREEN}/home/$commitsUser/.gitconfig\e[0m\n"
+
+    cat /home/$commitsUser/.gitconfig
+fi
+
+echo -e "\n"
+
+#printf "  %b Pi-hole DNS (IPv6): %s\\n" "${INFO}" "_____________"
+
+#wget -O basic-install.sh https://install.pi-hole.net
+#sudo bash basic-install.sh
